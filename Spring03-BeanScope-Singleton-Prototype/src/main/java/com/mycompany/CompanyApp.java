f16bc6c30265222b1e78a4bd95b86ca95ad1a049
Moses <moses.harvey532@gmail.com>
2022-05-13 12:25:21 +0200

    Singleton-Prototype

---
+++ Spring03-BeanScope-Singleton-Prototype/src/main/java/com/mycompany/CompanyApp.java
@@ -0,0 +1,20 @@
+package com.mycompany;
+
+
+
+import com.mycompany.interfaces.Course;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class CompanyApp {
+    public static void main(String[] args) {
+        ApplicationContext container = new ClassPathXmlApplicationContext("config.xml");
+        Course course1 = container.getBean("java", Course.class);
+        Course course2 = container.getBean("java", Course.class);
+
+        System.out.println("Pointing to the same objects: "+(course1 == course2));
+        System.out.println("Memory Location for course1"+course1);
+        System.out.println("Memory Location for course1"+course2);
+
+    }
+}
---
+++ Spring03-BeanScope-Singleton-Prototype/src/main/java/com/mycompany/services/Java.java
@@ -0,0 +1,22 @@
+package com.mycompany.services;
+
+
+import com.mycompany.interfaces.Course;
+
+public class Java implements Course {
+
+//    OfficeHours officeHours;
+//
+//    public OfficeHours getOfficeHours() {
+//        return officeHours;
+//    }
+//
+//    public void setOfficeHours(OfficeHours officeHours) {
+//        this.officeHours = officeHours;
+//    }
+
+
+    public void getTeachingHours() {
+        System.out.println("Weekly teaching hours: ");
+    }
+}
