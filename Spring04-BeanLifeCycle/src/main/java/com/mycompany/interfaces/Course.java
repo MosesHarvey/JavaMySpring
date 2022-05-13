3fe48e1daf85ab7908df7373965ac45b6889682c
Moses <moses.harvey532@gmail.com>
2022-05-13 13:10:23 +0200

    init-destroy methods

---
+++ Spring04-BeanLifeCycle/src/main/java/com/mycompany/interfaces/Course.java
@@ -0,0 +1,6 @@
+package com.mycompany.interfaces;
+
+public interface Course {
+
+    void getTeachingHours();
+}
---
+++ Spring04-BeanLifeCycle/src/main/java/com/mycompany/services/Java.java
@@ -0,0 +1,21 @@
+package com.mycompany.services;
+
+import com.mycompany.interfaces.Course;
+
+public class Java implements Course {
+
+    @Override
+    public void getTeachingHours() {
+        System.out.println("Weekly Teaching Hours: 20");
+    }
+
+    public void myInitMethod(){
+        System.out.println("Executing Init Method");
+
+
+    }
+
+    public void myDestroyMethod(){
+        System.out.println("Executing destroy method");
+    }
+}
