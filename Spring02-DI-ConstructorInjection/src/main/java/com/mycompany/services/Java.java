dc90403390353af1ebc160f057bb357374612cfe
Moses <moses.harvey532@gmail.com>
2022-05-13 11:34:44 +0200

    Constructor Injection -- Polymorphyism

--- Spring02-DI-ConstructorInjection/src/main/java/com/mycompany/services/Java.java
+++ Spring02-DI-ConstructorInjection/src/main/java/com/mycompany/services/Java.java
@@ -1,17 +1,26 @@
 package com.mycompany.services;
 
 import com.mycompany.interfaces.Course;
+import com.mycompany.interfaces.ExtraSessions;
 
 public class Java implements Course {
 
-    OfficeHours officeHours;
+//    OfficeHours officeHours;
+//
+//   public Java(OfficeHours officeHours){
+//       this.officeHours =officeHours;
+//   }
 
-   public Java(OfficeHours officeHours){
-       this.officeHours =officeHours;
-   }
+
+
+    ExtraSessions extraSessions;
+
+    public Java(ExtraSessions extraSessions){
+        this.extraSessions =extraSessions;
+    }
 
     @Override
     public void getTeachingHours() {
-        System.out.println("Weekly teaching hours: "+(20+ officeHours.getHours()));
+        System.out.println("Weekly teaching hours: "+(20+ extraSessions.getHours()));
     }
 }
