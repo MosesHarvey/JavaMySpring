dff2c21d5dab4b5bda517ad8e4adf208fd9bcf81
Moses <moses.harvey532@gmail.com>
2022-08-20 01:34:00 +0200

    Autowired- 3 types of injection

---
+++ Spring04-JavaAnnotation-Autowired/src/main/java/com/mycompany/services/Java.java
@@ -0,0 +1,44 @@
+package com.mycompany.services;
+
+import com.mycompany.interfaces.Course;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class Java implements Course {
+
+
+    // Field injection
+    @Autowired
+    private OfficeHours officeHours;
+
+    // constructor injection
+//    @Autowired
+//    public Java(OfficeHours officeHours) {
+//        this.officeHours = officeHours;
+//    }
+
+// this dosen't work without injection using autowired
+//    public void setOfficeHours(OfficeHours officeHours) {
+//        this.officeHours = officeHours;
+//    }
+
+
+    public OfficeHours getOfficeHours() {
+        return officeHours;
+    }
+
+    // setter injection
+//    @Autowired
+//    public void setOfficeHours(OfficeHours officeHours) {
+//        this.officeHours = officeHours;
+//    }
+
+    @Override
+    public void getTeachingHours() {
+        System.out.println("Weekly Teaching Hours: " + (20+ officeHours.getHours()));
+//        System.out.println("Weekly Teaching Hours:  15" );
+
+    }
+}
