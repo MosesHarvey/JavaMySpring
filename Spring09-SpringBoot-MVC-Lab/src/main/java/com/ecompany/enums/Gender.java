9eb599957c2348acb312d5195a0b628ee50d80a8
Moses <moses.harvey532@gmail.com>
2022-08-20 21:01:46 +0200

    Spring Boot MVC Employee Create

---
+++ Spring09-SpringBoot-MVC-Lab/src/main/java/com/ecompany/enums/Gender.java
@@ -0,0 +1,6 @@
+package com.ecompany.enums;
+
+public enum Gender {
+    Male,
+    Female
+}
---
+++ Spring09-SpringBoot-MVC-Lab/src/test/java/com/ecompany/Spring09SpringBootMvcLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring09SpringBootMvcLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
