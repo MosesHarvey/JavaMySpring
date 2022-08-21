1423f9720065ca2ae1f2dbc33c2d3d13d164d619
Moses <moses.harvey532@gmail.com>
2022-08-21 23:51:01 +0200

    User Update

--- Spring10-MVC-Task-Management/src/main/java/com/etask/controller/UserController.java
+++ Spring10-MVC-Task-Management/src/main/java/com/etask/controller/UserController.java
@@ -1,28 +1,63 @@
 package com.etask.controller;
 
+import com.etask.dto.RoleDTO;
 import com.etask.dto.UserDTO;
+import com.etask.service.RoleService;
+import com.etask.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
+    @Autowired
+    RoleService roleService;
+
+    @Autowired
+    UserService userService;
     @GetMapping({"/create", "/add", "/initialize"})
     public String createUser(Model model){
 
     model.addAttribute("user", new UserDTO());
+    model.addAttribute("roles", roleService.findAll());
+    model.addAttribute("users", userService.findAll());
+
         return "/user/user-create";
     }
 
     @PostMapping("/create")
-    public String userCreate(){
+    public String insertUser(UserDTO user, Model model){
+
+       userService.save(user);
+        // we need user. role table-users
+        model.addAttribute("user", new UserDTO());
+        model.addAttribute("roles", roleService.findAll());
+        model.addAttribute("users", userService.findAll());
 
         return "/user/user-create";
 
     }
 
+    @GetMapping("/update/{username}")
+    public String editUser(@PathVariable("username") String username, Model model){
+        model.addAttribute("user", userService.findById(username));
+        model.addAttribute("users", userService.findAll());
+        model.addAttribute("roles", roleService.findAll());
+        return "/user/update";
+    }
+
+    @PostMapping("/update/{username}")
+    public String updateUser(@PathVariable("username") String username, UserDTO user, Model model) {
+
+        userService.update(user);
+
+        model.addAttribute("user", new UserDTO());
+        model.addAttribute("roles", roleService.findAll());
+        model.addAttribute("users", userService.findAll());
+
+        return "/user/user-create";
+    }
 }
