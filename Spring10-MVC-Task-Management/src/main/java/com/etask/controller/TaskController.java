ddfc66eeecbb71aac1fe700d34c359ab92f4583e
Moses <moses.harvey532@gmail.com>
2022-08-23 18:49:59 +0200

    task update

--- Spring10-MVC-Task-Management/src/main/java/com/etask/controller/TaskController.java
+++ Spring10-MVC-Task-Management/src/main/java/com/etask/controller/TaskController.java
@@ -2,17 +2,13 @@ package com.etask.controller;
 
 import com.etask.dto.TaskDTO;
 import com.etask.enums.Status;
-import com.etask.implementation.TaskServiceImpl;
 import com.etask.service.ProjectService;
 import com.etask.service.TaskService;
 import com.etask.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.UUID;
@@ -56,5 +52,22 @@ public class TaskController{
         taskService.deleteById(id);
         return "redirect:/task/create";
 
+    }
+
+    @GetMapping("/update/{id}")
+    public String editTask(@PathVariable("id") Long id, Model model){
+
+        model.addAttribute("task",taskService.findById(id));
+        model.addAttribute("projects", projectService.findAll());
+        model.addAttribute("employees", userService.findEmployees());
+        model.addAttribute("tasks",taskService.findAll());
+
+        return "/task/update";
+    }
+
+    @PostMapping("/update/{id}")
+    public String updateTask(TaskDTO task, Model model){
+        taskService.update(task);
+        return "redirect:/task/create";
     }
 }
