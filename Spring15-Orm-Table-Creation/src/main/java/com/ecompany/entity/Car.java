2128c0245e523bca580a13715f393a160ae102e8
Moses <moses.harvey532@gmail.com>
2022-09-01 10:03:37 +0200

    postgress -- annotations

--- Spring15-Orm-Table-Creation/src/main/java/com/ecompany/entity/Car.java
+++ Spring15-Orm-Table-Creation/src/main/java/com/ecompany/entity/Car.java
@@ -1,16 +1,20 @@
 package com.ecompany.entity;
 
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
+@Table(name="cars")
 public class Car {
 
     @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
     private Long id;
 
     private String make;
     private String model;
+
+
 
 }
--- Spring15-Orm-Table-Creation/src/main/resources/data.sql
+++ Spring15-Orm-Table-Creation/src/main/resources/data.sql
@@ -1,6 +1,6 @@
-INSERT INTO student(id, first_name, last_name, email) VALUES (1, 'Mike', 'Smith', 'mike.smith@abc.com');
-INSERT INTO student(id, first_name, last_name, email) VALUES (2, 'Mile', 'Tyson', 'mike.tyson@abc.com');
-INSERT INTO student(id, first_name, last_name, email) VALUES (3, 'Melenda', 'Smith', 'Melenda.smith@abc.com');
-INSERT INTO student(id, first_name, last_name, email) VALUES (4, 'Black', 'Smith', 'black.smith@abc.com');
-
-INSERT INTO car(id, make, model) VALUES (5, 'Honda','SUV');
+-- INSERT INTO student(first_name, last_name, email) VALUES ('Mike', 'Smith', 'mike.smith@abc.com');
+-- INSERT INTO student(first_name, last_name, email) VALUES ('Mile', 'Tyson', 'mike.tyson@abc.com');
+-- INSERT INTO student(first_name, last_name, email) VALUES ('Melenda', 'Smith', 'Melenda.smith@abc.com');
+-- INSERT INTO student(first_name, last_name, email) VALUES ('Black', 'Smith', 'black.smith@abc.com');
+--
+-- INSERT INTO car(make, model) VALUES ('Honda','SUV');
