65b3e2dc9352d73a3d376f67ef230e87b1afaf93
Moses <moses.harvey532@gmail.com>
2022-09-08 16:55:43 +0200

    Security - RoleBase

--- Spring28-Security-HttpBase-RoleBase/src/main/java/securityhttpbaserolebase/security/SecurityConfiguration.java
+++ Spring28-Security-HttpBase-RoleBase/src/main/java/securityhttpbaserolebase/security/SecurityConfiguration.java
@@ -17,6 +17,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected  void configure(HttpSecurity http) throws Exception{
         http
                 .authorizeRequests() // request should be authorized
+                .antMatchers("index.html").permitAll()
+                .antMatchers("/profile/**").authenticated()
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/management.**").hasAnyRole("ADMIN", "MANAGER")
                 .anyRequest().authenticated() // incoming request be authenticated
                 .and()
                 .httpBasic();  // perform basic authentication
@@ -28,7 +32,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         auth
                 .inMemoryAuthentication()
                 .withUser("admin").password(passwordEncoder().encode("admin123")).roles("ADMIN").and()
-                .withUser("moses").password(passwordEncoder().encode("moses123")).roles("USER");
+                .withUser("moses").password(passwordEncoder().encode("moses123")).roles("USER")
+                .and()
+                .withUser("manager").password(passwordEncoder().encode("manager123")).roles("MANAGER");
     }
 
     @Bean
