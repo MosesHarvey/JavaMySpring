53b7f2578f5d106310b47a33cf26fbc4a814f3a1
Moses <moses.harvey532@gmail.com>
2022-09-08 08:34:19 +0200

    Spring security 1

--- Spring22-ORM-Task-Management/src/main/java/com/etask/enums/MovieType.java
+++
@@ -1,4 +0,0 @@
-package com.etask.enums;
-
-public enum MovieType {
-}
--- Spring22-ORM-Task-Management/src/main/java/com/etask/repository/ProjectRepository.java
+++ Spring22-ORM-Task-Management/src/main/java/com/etask/repository/ProjectRepository.java
@@ -3,9 +3,11 @@ package com.etask.repository;
 import com.etask.entity.Project;
 import com.etask.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ProjectRepository extends JpaRepository<Project,Long> {
 
     Project findByProjectCode(String code);
