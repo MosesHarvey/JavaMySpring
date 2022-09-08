3dc4f46565e63978ea54e19f92b236ebb1168c2c
Moses <moses.harvey532@gmail.com>
2022-09-08 09:06:22 +0200

    Spring security - Custom Configuration

---
+++ Spring27-Security-Custom-Configuration/src/test/java/com/customconfig/Spring27SecurityCustomConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.customconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring27SecurityCustomConfigurationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
