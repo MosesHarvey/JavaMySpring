cd9211d5e2438fe53828a508abd90e5853f2eed9
Moses <moses.harvey532@gmail.com>
2022-09-08 16:36:28 +0200

    Security - HttpBasic, RoleBase

---
+++ Spring28-Security-HttpBase-RoleBase/src/main/java/securityhttpbaserolebase/SecurityHttpBaseRoleBaseApplication.java
@@ -0,0 +1,13 @@
+package securityhttpbaserolebase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityHttpBaseRoleBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityHttpBaseRoleBaseApplication.class, args);
+    }
+
+}
---
+++ Spring28-Security-HttpBase-RoleBase/src/test/java/securityhttpbaserolebase/Spring28SecurityHttpBaseRoleBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package securityhttpbaserolebase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring28SecurityHttpBaseRoleBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
