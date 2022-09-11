de5f3d7bc5bd459517b8d2ac9a68748c17203d21
Moses <moses.harvey532@gmail.com>
2022-09-11 16:15:50 +0200

    School lab Rest -- consuming weather api

--- Spring33-Rest-SchoolLab/src/main/java/com/schoollab/controller/ApiController.java
+++ Spring33-Rest-SchoolLab/src/main/java/com/schoollab/controller/ApiController.java
@@ -1,31 +1,35 @@
 package com.schoollab.controller;
 
 
+import com.schoollab.model.Address;
 import com.schoollab.model.ResponseWrapper;
 import com.schoollab.model.Teacher;
+import com.schoollab.repository.AddressRepository;
 import com.schoollab.repository.ParentRepository;
 import com.schoollab.repository.StudentRepository;
 import com.schoollab.repository.TeacherRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.Scanner;
 
 @RestController
 public class ApiController {
 
-      private TeacherRepository teacherRepository;
+    private TeacherRepository teacherRepository;
     private StudentRepository studentRepository;
     private ParentRepository parentRepository;
+    private AddressRepository addressRepository;
 
-    public ApiController(TeacherRepository teacherRepository, StudentRepository studentRepository, ParentRepository parentRepository) {
+    public ApiController(TeacherRepository teacherRepository, StudentRepository studentRepository, ParentRepository parentRepository, AddressRepository addressRepository) {
         this.teacherRepository = teacherRepository;
         this.studentRepository = studentRepository;
         this.parentRepository = parentRepository;
+        this.addressRepository = addressRepository;
     }
 
     @GetMapping("/teachers")
@@ -46,6 +50,18 @@ public class ApiController {
              HttpStatus.ACCEPTED.value(),
              parentRepository.findAll());
      return ResponseEntity.status(HttpStatus.ACCEPTED).body(responseWrapper);
+    }
+
+    @PutMapping("/address/{id}")
+    public Address updateAddress(@PathVariable("id") Long id, @RequestBody Address address) throws Exception {
+        Optional<Address> foundAddress = addressRepository.findById(id);
+
+        if(!foundAddress.isPresent()){
+            throw new Exception("Address does not exists!");
+        }
+        address.setCurrentTemperature(new Address().consumeTemp(address.getCity()));
+        address.setId(foundAddress.get().getId());
+        return addressRepository.save(address);
     }
 
 }
