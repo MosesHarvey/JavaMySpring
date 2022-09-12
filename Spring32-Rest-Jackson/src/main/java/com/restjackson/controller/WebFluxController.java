0816d55666b6a90b2b27461f9d774ccdce3260cf
Moses <moses.harvey532@gmail.com>
2022-09-12 08:09:46 +0200

    web client example

--- Spring32-Rest-Jackson/src/main/java/com/restjackson/controller/WebFluxController.java
+++ Spring32-Rest-Jackson/src/main/java/com/restjackson/controller/WebFluxController.java
@@ -5,12 +5,15 @@ import com.restjackson.entity.MovieCinema;
 import com.restjackson.repository.GenreRepository;
 import com.restjackson.repository.MovieCinemaRepository;
 import com.restjackson.repository.MovieRepository;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.util.List;
+
+
 
 @RestController
 public  class WebFluxController {
@@ -57,6 +60,55 @@ public  class WebFluxController {
     public Mono<Void>deleteGenre(@PathVariable("id") Long id){
         genreRepository.deleteById(id);
         return Mono.empty();
+    }
+
+    // ================= WebClient Example ====================
+
+    @GetMapping("/flux")
+    public Flux<MovieCinema>readWithWebClient(){
+        return webClient.get()
+                .uri("/flux-movie-cinemas")
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .retrieve().bodyToFlux(MovieCinema.class);
+    }
+
+    @GetMapping("/mono/{id}")
+    public Mono<MovieCinema>readMonoWithWebClient(@PathVariable("id") Long id){
+        return webClient
+                .get()
+                .uri("/mono-movie-cinema/{id}", id)
+                .retrieve().bodyToMono(MovieCinema.class);
+
+    }
+
+    @GetMapping("/mono-rp")
+    public Mono<MovieCinema>readMonoWithWebClientRequestParam(@RequestParam("id") Long id){
+        return webClient
+                .get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/mono-movie-cinema")
+                        .queryParam("id").build())
+                .retrieve()
+                .bodyToMono(MovieCinema.class);
+    }
+
+    @PostMapping("/create")
+    public Mono<Genre>createWecClient(@RequestBody Genre genre){
+        return webClient
+                .post()
+                .uri("/create-genre")
+                .body(Mono.just(genre), Genre.class)
+                .retrieve()
+                .bodyToMono(Genre.class);
+
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public Mono<Void> deleteWebClient(@PathVariable("id") Long id){
+        return webClient.delete()
+                .uri("/delete-genre/{id}", id)
+                .retrieve()
+                .bodyToMono(Void.class);
     }
 
 
