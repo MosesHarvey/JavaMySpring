6bc6c7fdb82d931c0943ded84fcb6de85066d5b1
Moses <moses.harvey532@gmail.com>
2022-09-13 16:42:39 +0200

    Rest Security JWT

---
+++ Spring37-Rest-Security-JWT/src/main/java/com/restsecurityjwt/RestSecurityJwtApp.java
@@ -0,0 +1,13 @@
+package com.restsecurityjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSecurityJwtApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestSecurityJwtApp.class, args);
+    }
+
+}
---
+++ Spring37-Rest-Security-JWT/src/test/java/com/restsecurityjwt/RestSecurityJwtAppTests.java
@@ -0,0 +1,13 @@
+package com.restsecurityjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSecurityJwtAppTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
