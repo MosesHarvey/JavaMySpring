a6f2b76c3c90b90999cb56d0ed4325b9c58e2b58
Moses <moses.harvey532@gmail.com>
2022-10-19 18:37:30 +0200

    Checking account

--- Spring46-Unit-Testing-Junit5/src/main/java/CheckingAccount.java
+++ Spring46-Unit-Testing-Junit5/src/main/java/CheckingAccount.java
@@ -1,2 +1,33 @@
 public class CheckingAccount {
+
+    double balance;
+    long accountNumber;
+    String accountHolder;
+    String type = "checking";
+
+
+    public void  setInfo(double balance, long accountNumber, String accountHolder) {
+        this.balance = balance;
+        this.accountNumber = accountNumber;
+        this.accountHolder = accountHolder;
+    }
+
+    public double deposit(double amount){
+        balance+=amount;
+        return balance;
+    }
+
+    public double withdraw(double amount){
+        if(amount<=balance) balance-=amount;
+        else System.out.println("Insufficient fund in account");
+
+        return balance;
+    }
+
+    public double purchase(String item, double price){
+        if(price<=balance) balance-=price;
+        else balance-=(price+35);
+
+        return  balance;
+    }
 }
